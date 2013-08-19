036e6d6b5a87003ee5779f25a582782834307845
ocefpaf <ocefpaf@gmail.com>
2013-08-19 19:21:11 -0300

    Using absolute imports.

--- mlabwrap/__init__.py
+++ mlabwrap/__init__.py
@@ -1,3 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from matlabpipe import MatlabPipe, MatlabError, MatlabConnectionError
+from __future__ import absolute_import
+
+from .matlabpipe import MatlabPipe, MatlabError, MatlabConnectionError
+
+__version__ = '0.1.0'
